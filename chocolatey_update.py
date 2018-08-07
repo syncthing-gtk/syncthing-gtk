c5f037fa780986d366d43546a93b94a145979dec
Daniel Alley <dalley@redhat.com>
2018-08-07 09:03:07 -0400

    Change 'shebangs' to Python 3
    Deliberately exclude Caja and Nemo plugins on the assumption that
    they're still Python 2, although I'm not sure about that.

--- chocolatey_update.py
+++ chocolatey_update.py
@@ -14,7 +14,11 @@ Requirements:
 from __future__ import unicode_literals, print_function
 
 import re, os, json
-from urllib import request # Py3
+try:
+	from urllib import request # Py3
+except ImportError:
+	import urllib2 as request  # Py2
+	from io import open
 
 
 print("Retrieving last version...")
