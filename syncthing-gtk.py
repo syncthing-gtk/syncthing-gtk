3b5286e677d475006cc7d8d8e5b7ac6017cc1d12
Andrej Shadura <andrew.shadura@collabora.co.uk>
2022-12-05 16:12:44 +0100

    Prevent Gdk 4.0 from being loaded with Gtk 3.0

--- syncthing-gtk.py
+++ syncthing-gtk.py
@@ -3,11 +3,12 @@ import os, sys, gi
 
 if __name__ == "__main__":
 	gi.require_version('Gtk', '3.0')
+	gi.require_version('Gdk', '3.0')
 	gi.require_version('Rsvg', '2.0')
-	
+
 	from syncthing_gtk.tools import init_logging, init_locale, IS_WINDOWS
 	init_logging()
-	
+
 	if IS_WINDOWS:
 		from syncthing_gtk.windows import (
 			enable_localization,
@@ -20,11 +21,11 @@ if __name__ == "__main__":
 			os.environ["GTK_THEME"] = "Adwaita:dark"
 		if config["language"] not in ("", "None", None):
 			os.environ["LANGUAGE"] = config["language"]
-		
+
 		enable_localization()
-	
+
 	init_locale("locale/")
-	
+
 	if IS_WINDOWS:
 		from syncthing_gtk.windows import enable_localization
 		fix_localized_system_error_messages()
@@ -32,10 +33,10 @@ if __name__ == "__main__":
 		from gi.repository import Gtk
 		Gtk.IconTheme.get_default().prepend_search_path(os.path.abspath(os.path.join(os.getcwd(), "icons", "32x32", "apps")))
 		Gtk.IconTheme.get_default().prepend_search_path(os.path.abspath(os.path.join(os.getcwd(), "icons")))
-	
+
 	from gi.repository import Gtk
 	Gtk.IconTheme.get_default().prepend_search_path(os.path.join(os.getcwd(), "icons"))
 	Gtk.IconTheme.get_default().prepend_search_path(os.path.join(os.getcwd(), "icons/32x32/status"))
-	
+
 	from syncthing_gtk.app import App
 	App("./glade", "./icons").run(sys.argv)
