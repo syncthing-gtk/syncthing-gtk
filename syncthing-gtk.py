4edc3584aebb34807d243e296e5bede648add1ac
Anton Lazarev <antonok35@gmail.com>
2023-06-19 22:25:30 -0700

    bump Gtk version requirement

--- syncthing-gtk.py
+++ syncthing-gtk.py
@@ -11,8 +11,8 @@ from typing import Optional
 
 import gi
 
-gi.require_version("Gtk", "3.0")
-gi.require_version("Gdk", "3.0")
+gi.require_version("Gtk", "4.0")
+gi.require_version("Gdk", "4.0")
 gi.require_version("Rsvg", "2.0")
 
 # pylint: disable=wrong-import-position
