b4185f663ad35bd3c9334e758c5a9b0653bed958
Félix Piédallu <felix.piedallu@non.se.com>
2023-06-27 14:09:59 +0200

    pylint: small fix in scripts/

--- scripts/syncthing-plugin-caja.py
+++ scripts/syncthing-plugin-caja.py
@@ -4,21 +4,21 @@ Caja plugin for Syncthing-GTK
 See syncthing_gtk/nautilusplugin.py for more info
 """
 # pylint: disable=invalid-name
-
 import os
-
 from gi.repository import Caja  # pylint: disable=no-name-in-module
+
+from syncthing_gtk.nautilusplugin import NautiluslikeExtension
+
+NautiluslikeExtension.set_plugin_module(Caja)
+
+
+class CajaNautilus(NautiluslikeExtension, Caja.InfoProvider, Caja.MenuProvider):
+    """Browser extension wrapper"""
+
 
 # Setting this environment variable will prevent __init__ in
 # syncthing_gtk package from loading stuff that depends on GTK3-only
 # features. It probably breaks other modules in most horrible ways,
 # but they are not going to be used anyway
-from syncthing_gtk.nautilusplugin import NautiluslikeExtension
 
 os.environ["GTK2APP"] = "1"
-
-
-NautiluslikeExtension.set_plugin_module(Caja)
-
-class CajaNautilus(NautiluslikeExtension, Caja.InfoProvider, Caja.MenuProvider):
-    """Browser extension wrapper"""
