3358e2d86af6f11b1f03ee9cd00bfc1abea2d214
Félix Piédallu <felix.piedallu@non.se.com>
2023-07-28 14:12:30 +0200

    Fix build_msi: clusterf*ck with windows paths

--- packaging/windows/build_msi.sh
+++ packaging/windows/build_msi.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
-set -x
+# set -x
 
 BUILDDIR="$PWD/_build_msi"
 DESTDIR="$BUILDDIR/installdir"
@@ -11,8 +11,21 @@ meson setup "$BUILDDIR" \
 
 DESTDIR="$DESTDIR" ninja -C "$BUILDDIR" install
 
+PREFIX=$(meson introspect --buildoptions "$BUILDDIR" | jq -r '.[] | select(.name=="prefix") | .value')
+
+INSTPREFIX="$DESTDIR/${PREFIX//C:\//}/"
+
 # List installed files
-find "$DESTDIR" | wixl-heat -p "$DESTDIR/usr/local/" \
+readarray -t FILES < <(find "$INSTPREFIX")
+
+# Convert to windows paths if necessary
+if [ "$MSYSTEM" == "MINGW64" ]; then
+    INSTPREFIX="$(cygpath -w "$INSTPREFIX")"
+    readarray -t FILES < <(cygpath -w "${FILES[@]}")
+fi
+
+# Generate the xml containing the list of files
+printf '%s\n' "${FILES[@]}" | wixl-heat -p "$INSTPREFIX" \
     --component-group CG.syncthing-gtk \
     --directory-ref INSTALLDIR \
     --var var.SourceDir \
@@ -22,7 +35,7 @@ find "$DESTDIR" | wixl-heat -p "$DESTDIR/usr/local/" \
 # Generate the MSI file
 wixl -v --ext ui --arch x64 \
     --extdir /mingw64/share/wixl-0.102/ext \
-    -D SourceDir="$DESTDIR/usr/local" \
+    -D SourceDir="$INSTPREFIX" \
     -D ProductIcon="$PWD/icons/st-logo-128.ico" \
     "$BUILDDIR/packaging/windows/syncthing-gtk.wxs" \
     "$BUILDDIR/packaging/windows/msi_files.wxs" \
