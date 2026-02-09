a273c70c561b6efa785d9fc816452342ae97030c
Félix Piédallu <felix@piedallu.me>
2026-02-09 14:05:37 +0100

    packaging: fix build_msi.sh

--- packaging/windows/build_msi.sh
+++ packaging/windows/build_msi.sh
@@ -11,9 +11,13 @@ meson setup "$BUILDDIR" \
 
 DESTDIR="$DESTDIR" ninja -C "$BUILDDIR" install
 
-PREFIX=$(meson introspect --buildoptions "$BUILDDIR" | jq -r '.[] | select(.name=="prefix") | .value')
+PREFIX=$(
+    meson introspect --buildoptions "$BUILDDIR" \
+    | jq -r '.[] | select(.name=="prefix") | .value' \
+    | sed 's|C:/||' | sed 's|D:/||'
+)
 
-INSTPREFIX="$DESTDIR/${PREFIX//C:\//}/"
+INSTPREFIX="$DESTDIR/$PREFIX/"
 
 # List installed files
 readarray -t FILES < <(find "$INSTPREFIX")
@@ -34,7 +38,7 @@ printf '%s\n' "${FILES[@]}" | wixl-heat -p "$INSTPREFIX" \
 
 # Generate the MSI file
 wixl -v --ext ui --arch x64 \
-    --extdir /mingw64/share/wixl-0.102/ext \
+    --extdir /mingw64/share/wixl-0.106/ext \
     -D SourceDir="$INSTPREFIX" \
     -D ProductIcon="$PWD/icons/st-logo-128.ico" \
     "$BUILDDIR/packaging/windows/syncthing-gtk.wxs" \
